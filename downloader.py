11a27c1272ca8d69fd46e76b7a6135e08a61a198
Alessio Orsini <alessiorsini.ao@proton.me>
2022-11-14 18:28:56 +0100

    Now works if artist has less than 6 albums.

--- downloader.py
+++ downloader.py
@@ -7,7 +7,7 @@ from loguru import logger
 base_link = "https://genius.com"
 
 def getLinkName(name):
-    return name.replace("(", "").replace(")", "").replace("&", "and").replace(" ", "-")
+    return name.replace("(", "").replace(")", "").replace("&", "and").replace(" ", "-").replace(".", '.')
 
 def getValidFilename(name):
     invalid = '!\"$%#/\0'
@@ -36,38 +36,56 @@ def main(artist: str, debug: bool):
         level = 'DEBUG' if debug else 'INFO'
     )
 
-    try: os.mkdir(artist)
-    except FileExistsError: pass
+    os.makedirs(f"lyrics/{artist}", exist_ok = True)
 
     logger.debug(f"making request to {base_link}/artists/{getLinkName(artist)}")
     artist_page = req.get(f"{base_link}/artists/{getLinkName(artist)}").text
-    albums_list_link = artist_page[artist_page.find("/artists/albums?for_artist_page="):].split('\"')[0]
 
-
-    logger.debug(f"making request to {base_link}/{albums_list_link}")
-    albums_list_page = req.get(f"{base_link}{albums_list_link}").text
-    pattern = '<li><a href=\"/albums/'
-    logger.debug(f"Number of '{pattern}' found: {albums_list_page.count(pattern)}")
 
     logger.info("Searching for albums...")
     albums_list = []
-    for i in range(albums_list_page.count(pattern)):
-        useful = albums_list_page[albums_list_page.find(pattern):]
+    if artist_page.find("/artists/albums?for_artist_page=") != -1:
+        logger.debug("artisty has more than 6 albums, using albums list page")
+        albums_list_link = artist_page[artist_page.find("/artists/albums?for_artist_page="):].split('\"')[0]
 
-        albums_list.append({
-            'name': getValidFilename(useful.split('>')[2].split('<')[0]),
-            'link': useful.split('\"')[1]
-        })
+        logger.debug(f"making request to {base_link}/{albums_list_link}")
+        albums_list_page = req.get(f"{base_link}{albums_list_link}").text
 
-        logger.debug(f"found album {albums_list[-1]['name']}")
-        albums_list_page = useful[2:]
+        pattern = '<li><a href=\"/albums/'
+        for i in range(albums_list_page.count(pattern)):
+            useful = albums_list_page[albums_list_page.find(pattern):]
+
+            albums_list.append({
+                'name': getValidFilename(useful.split('>')[2].split('<')[0]),
+                'link': base_link + useful.split('\"')[1]
+            })
+
+            logger.debug(f"found album {albums_list[-1]['name']}")
+            albums_list_page = useful[2:]
+    else:
+        logger.debug("Artist has less than 6 albums, using albums preview")
+
+        for i in range(artist_page.count('class="vertical_album_card"')):
+            useful = artist_page[artist_page[:artist_page.find('class="vertical_album_card"')].rfind("href="):].split('"')
+
+            albums_list.append({
+                'name': getValidFilename(useful[3]),
+                'link': useful[1]
+            })
+
+
+            artist_page = artist_page[artist_page.find('class="vertical_album_card"') + 1:]
+    
+    logger.debug(f"Number of albums found: {len(albums_list)}")
 
     for album in albums_list:
         logger.info(f"downloading album {album['name']}...")
 
-        try: os.mkdir(f"{artist}/{album['name']}")
+        try: os.mkdir(f"lyrics/{artist}/{album['name']}")
         except FileExistsError: pass
-        album_page = req.get(f"{base_link}{album['link']}").text
+
+        logger.debug(f"Making request to {album['link']}")
+        album_page = req.get(f"{album['link']}").text
 
         pattern = "u-display_block"
         logger.debug(f"number of '{pattern}' found: {album_page.count(pattern)}")
@@ -84,7 +102,7 @@ def main(artist: str, debug: bool):
             logger.debug(f"making request to {song_page_link}")
             song_page = req.get(song_page_link).text
 
-            with open(f"{artist}/{album['name']}/{song_name}.txt", 'w') as file:
+            with open(f"lyrics/{artist}/{album['name']}/{song_name}.txt", 'w') as file:
                 lyrics = ''
                 for i in range(song_page.count('<div data-lyrics-container="true"')):
                     song_page = song_page[song_page.find('<div data-lyrics-container="true"'):]
