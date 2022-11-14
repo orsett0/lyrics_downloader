a12e76de92e4743d033c55c837482fc7a9c868f3
Alessio Orsini <alessiorsini.ao@proton.me>
2022-11-15 00:26:34 +0100

    Ability to download just a song or album.

--- downloader.py
+++ downloader.py
@@ -13,6 +13,87 @@ def getValidFilename(name):
     invalid = '!\"$%#/\0'
     return "".join([ c for c in name if c not in invalid ])
 
+def getArtistPage(artist):
+    logger.debug(f"making request to {base_link}/artists/{getLinkName(artist)}")
+    return req.get(f"{base_link}/artists/{getLinkName(artist)}").text
+
+def getAlbumsList(artist_page):
+    logger.info("Searching for albums...")
+
+    pattern = 'class="vertical_album_card"'
+    name_pos = 3
+
+    if artist_page.find("/artists/albums?for_artist_page=") != -1:
+        logger.debug("artist has more than 6 albums, loading albums list page")
+        
+        albums_list_link = artist_page[artist_page.find("/artists/albums?for_artist_page="):].split('"')[0]
+        
+        logger.debug(f"making request to {base_link}/{albums_list_link}")
+        artist_page = req.get(f"{base_link}{albums_list_link}").text
+
+        pattern = 'class="album_link"'
+        name_pos = 4
+    
+    albums_list = []
+    for i in range(artist_page.count(pattern)):
+        useful = artist_page[artist_page[:artist_page.find(pattern)].rfind("href="):].split('"')
+
+        albums_list.append({
+            'name': getValidFilename(useful[name_pos].split("<")[0].replace(">", "")),
+            'link': ("" if base_link in useful[1] else base_link) + useful[1] 
+        })
+
+        artist_page = artist_page[artist_page.find(pattern) + 1:]
+    
+    logger.debug(f"Number of albums found: {len(albums_list)}")
+    return albums_list
+
+def getSongsList(album):
+    logger.debug(f"getting songs list for {album['name']}")
+
+    songs = []
+
+    logger.debug(f"Making request to {album['link']}")
+    album_page = req.get(album['link']).text
+    
+    pattern = "u-display_block"
+    logger.debug(f"number of songs found: {album_page.count(pattern)}")
+    for i in range(album_page.count(pattern)):
+        useful = album_page[album_page[:album_page.find(pattern)].rfind("href"):]
+
+        try: song_name = getValidFilename(useful.split(">")[2].split("<")[0].strip())
+        except IndexError: pass
+        try: song_page_link = useful.split('\"')[1]
+        except IndexError: pass
+        # ! I DONT FKING KNOW WHY IT THROWS AN INDEXERROR BUT IT STILL WORKS SO FCK IT
+
+        songs.append({
+            'name': song_name,
+            'link': song_page_link
+        })
+
+        album_page = useful[useful.find(pattern) + 1:]
+    
+    return songs
+
+def downloadSong(song_page_link):
+    logger.debug(f"making request to {song_page_link}")
+    song_page = req.get(song_page_link).text
+
+    pattern = '<div data-lyrics-container="true"'
+    lyrics = ''
+
+    for i in range(song_page.count(pattern)):
+        song_page = song_page[song_page.find(pattern):]
+        part = song_page[:song_page.find('</div>')]
+
+        for element in part.split('>'): 
+            if element.split('<')[0] != "": lyrics += f"\n{element.split('<')[0]}"
+
+        song_page = song_page[2:]
+
+    return html.unescape(lyrics)
+
 @click.command()
 @click.option(
     "--artist",
@@ -21,13 +102,27 @@ def getValidFilename(name):
     type = click.STRING
 )
 @click.option(
+    "--album",
+    "-A",
+    help = "Album da scaricare",
+    type = click.STRING,
+    default = None
+)
+@click.option(
+    "--song",
+    "-s",
+    help = "Canzone da scaricare",
+    type = click.STRING,
+    default = None
+)
+@click.option(
     "--debug",
     "-d",
     is_flag = True,
     default = False
 )
 
-def main(artist: str, debug: bool):
+def main(artist: str, album: str, song: str, debug: bool):
     logger.remove()
     logger.add(
         sys.stdout, 
@@ -36,87 +131,22 @@ def main(artist: str, debug: bool):
         level = 'DEBUG' if debug else 'INFO'
     )
 
-    os.makedirs(f"lyrics/{artist}", exist_ok = True)
+    artist_page = getArtistPage(artist)
+    albums_list = getAlbumsList(artist_page)
 
-    logger.debug(f"making request to {base_link}/artists/{getLinkName(artist)}")
-    artist_page = req.get(f"{base_link}/artists/{getLinkName(artist)}").text
+    for album_data in albums_list:
+        if album is not None and getLinkName(album) != getLinkName(album_data['name']): continue
+        logger.info(f"downloading album {album_data['name']}...")
 
+        os.makedirs(f"lyrics/{artist}/{album_data['name']}", exist_ok = True)
 
-    logger.info("Searching for albums...")
-    albums_list = []
-    if artist_page.find("/artists/albums?for_artist_page=") != -1:
-        logger.debug("artisty has more than 6 albums, using albums list page")
-        albums_list_link = artist_page[artist_page.find("/artists/albums?for_artist_page="):].split('\"')[0]
+        songs = getSongsList(album_data)
+        for song_data in songs:
+            if song is not None and getLinkName(song) != getLinkName(song_data['name']): continue
+            logger.debug(f"Downloading song {song_data['name']}")
 
-        logger.debug(f"making request to {base_link}/{albums_list_link}")
-        albums_list_page = req.get(f"{base_link}{albums_list_link}").text
-
-        pattern = '<li><a href=\"/albums/'
-        for i in range(albums_list_page.count(pattern)):
-            useful = albums_list_page[albums_list_page.find(pattern):]
-
-            albums_list.append({
-                'name': getValidFilename(useful.split('>')[2].split('<')[0]),
-                'link': base_link + useful.split('\"')[1]
-            })
-
-            logger.debug(f"found album {albums_list[-1]['name']}")
-            albums_list_page = useful[2:]
-    else:
-        logger.debug("Artist has less than 6 albums, using albums preview")
-
-        for i in range(artist_page.count('class="vertical_album_card"')):
-            useful = artist_page[artist_page[:artist_page.find('class="vertical_album_card"')].rfind("href="):].split('"')
-
-            albums_list.append({
-                'name': getValidFilename(useful[3]),
-                'link': useful[1]
-            })
-
-
-            artist_page = artist_page[artist_page.find('class="vertical_album_card"') + 1:]
-    
-    logger.debug(f"Number of albums found: {len(albums_list)}")
-
-    for album in albums_list:
-        logger.info(f"downloading album {album['name']}...")
-
-        try: os.mkdir(f"lyrics/{artist}/{album['name']}")
-        except FileExistsError: pass
-
-        logger.debug(f"Making request to {album['link']}")
-        album_page = req.get(f"{album['link']}").text
-
-        pattern = "u-display_block"
-        logger.debug(f"number of '{pattern}' found: {album_page.count(pattern)}")
-        for i in range(album_page.count(pattern)):
-            useful = album_page[album_page[:album_page.find(pattern)].rfind("href"):]
-
-            try: 
-                song_name = getValidFilename(useful.split(">")[2].split("<")[0].strip())
-                song_page_link = useful.split('\"')[1]
-            except IndexError:
-                # ! I DONT FKING KNOW WHY IT THROWS AN INDEXERROR BUT IT STILL WORKS SO FCK IT
-                pass
-
-            logger.debug(f"making request to {song_page_link}")
-            song_page = req.get(song_page_link).text
-
-            with open(f"lyrics/{artist}/{album['name']}/{song_name}.txt", 'w') as file:
-                lyrics = ''
-                for i in range(song_page.count('<div data-lyrics-container="true"')):
-                    song_page = song_page[song_page.find('<div data-lyrics-container="true"'):]
-                    part = song_page[:song_page.find('</div>')]
-
-                    for element in part.split('>'): 
-                        if element.split('<')[0] != "": lyrics += f"\n{element.split('<')[0]}"
-
-                    song_page = song_page[2:]
-
-                file.write(html.unescape(lyrics))
-
-            #logger.debug(f"pattern: {pattern}, useful[useful.find(pattern) + 1:102]: {useful[useful.find(pattern) + 1:102]}")
-            album_page = useful[useful.find(pattern) + 1:]
-
+            lyrics = downloadSong(song_data['link'])
+            with open(f"lyrics/{artist}/{album_data['name']}/{song_data['name']}.txt", 'w') as file:
+                file.write(lyrics)
 
 if __name__ == '__main__': main()
