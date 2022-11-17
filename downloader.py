9d52bd0a84b8e2b5e4fc069522c73585594aca35
Alessio Orsini <alessiorsini.ao@gmail.com>
2022-11-17 16:06:01 +0100

    added a license.

--- downloader.py
+++ downloader.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+#
+# Copyright (C) 2022 Alessio Orsini <alessiorsini.ao@proton.me>
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
 
 import click, os, sys, html, time
 import requests as req
